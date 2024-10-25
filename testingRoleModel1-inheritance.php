e8c47bfdcba9b9917fc3fd5e547b0bfcb8b193b7
Maulana-Dra <maulana.dra06@gmail.com>
2024-10-25 17:23:12 +0700

    nambah coment

--- testingRoleModel1-inheritance.php
+++ testingRoleModel1-inheritance.php
@@ -6,13 +6,14 @@ $obj_mobil[] = new User(1,"Toyota Camry","Sedan","4-Silinder","Maulana");
 $obj_mobil[] = new User(2,"Honda Civic","Sedan/Hatchback", "4-Silinder","Indah");
 $obj_mobil[] = new User(3,"Tesla Model 3","Sedan", "Listrik","Muthe");
 
-    foreach($obj_mobil as $mobils){
+    foreach($obj_mobil as $mobils){ 
+        //testr
     echo "ID Mobil: ".$mobils->mobil_id."<br>";
     echo "Nama Mobil: ".$mobils->mobil_name."<br>";
     echo "Description Mobil: ".$mobils->mobil_tipe."<br>";
     echo "status Mobil: ".$mobils->mobil_mesin."<br>";
     echo $mobils->user()."<br>"; 
-    
+
     echo "<br>";
     }
    
